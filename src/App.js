2b839bce5711b42165bfdf908f6cdbd88bd21381
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-05-04 22:13:35 -0400

    update menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
+
+const StickersPage = () => (
+  <div>
+    <h1>STICKERS PAGE!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/stickers" component={StickersPage} />
+      </Switch>
     </div>
   );
 }
