d840db7793be088bfcc1b3d8b79e02fb12f7ca74
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-05-04 22:53:17 -0400

    update app with shop page, collection preview component, and collection item components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/Homepage/Homepage';
-
-const StickersPage = () => (
-  <div>
-    <h1>STICKERS PAGE!</h1>
-  </div>
-);
+import Homepage from './pages/home/Homepage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/stickers" component={StickersPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
